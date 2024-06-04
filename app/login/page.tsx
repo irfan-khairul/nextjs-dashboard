78a4364b4ac3880c37514625286989888071bbc5
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-06-05 04:39:27 +0800

    Chapter 16

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import { Metadata } from 'next';
 
 export default function LoginPage() {
   return (
@@ -7,7 +8,7 @@ export default function LoginPage() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
-              <AcmeLogo />
+            <AcmeLogo />
           </div>
         </div>
         <LoginForm />
@@ -15,3 +16,7 @@ export default function LoginPage() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
